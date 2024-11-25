1c3ee9b3f99d452e5e3c2cbcbcd4ff3cc87094ae
72umesh <umeshpal8141@gmail.com>
2024-11-25 15:55:30 +0530

    First Commit- Project Done

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// ------------------- Hero section Buttons --------------------
+document.getElementById("myButton1").onclick = function (){
+    location.href ="./Navigation/AllProducts.html#mirroless"
+}
+document.getElementById("myButton2").onclick = function (){
+    location.href ="./Navigation/AllProducts.html#dslr"
+}
+document.getElementById("myButton3").onclick = function (){
+    location.href ="./Navigation/AllProducts.html#film"
+}
+document.getElementById("myButton4").onclick = function (){
+    location.href ="./Navigation/AllProducts.html#action"
+}
+document.getElementById("myButton5").onclick = function (){
+    location.href ="./Navigation/AllProducts.html#drones"
+}
